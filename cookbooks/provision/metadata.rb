ce8b6f632f9a04ea1341fd161d741ee6022a844e
suprovich82 <suprovich82@gmail.com>
2019-05-01 23:22:24 +0300

    Added jenkins installation/start recipe based on community cookbook

--- cookbooks/provision/metadata.rb
+++ cookbooks/provision/metadata.rb
@@ -9,6 +9,7 @@ chef_version '>= 13.0'
 
 depends 'yum-mysql-community', '~> 4.0.1'
 depends 'mysql', '~> 8.0'
+depends 'jenkins', '~> 7.0.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
---
+++ cookbooks/provision/recipes/jenkins.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: provision
+# Recipe:: jenkins
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+include_recipe 'jenkins::java'
+include_recipe 'jenkins::master'
