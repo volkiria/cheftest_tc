8119234ce15cf1b9dbc757d3ca8e68b472af1ed2
suprovich82 <suprovich82@gmail.com>
2019-05-02 12:00:32 +0300

    Fixed issues reported by cookstyle and foodcritic

--- cookbooks/provision/recipes/hello-world.rb
+++ cookbooks/provision/recipes/hello-world.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 file '/tmp/Hello_World.txt' do
-    mode '0600'
-    owner 'root'
-    group 'root'
+  mode '0600'
+  owner 'root'
+  group 'root'
 end
--- cookbooks/provision/recipes/mysql-server.rb
+++ cookbooks/provision/recipes/mysql-server.rb
@@ -7,8 +7,8 @@
 include_recipe 'yum-mysql-community::mysql57'
 
 mysql_service 'cheftest' do
-    port '3306'
-    version '5.7'
-    initial_root_password 'change_me'
-    action [:create, :start]
+  port '3306'
+  version '5.7'
+  initial_root_password 'change_me'
+  action [:create, :start]
 end
